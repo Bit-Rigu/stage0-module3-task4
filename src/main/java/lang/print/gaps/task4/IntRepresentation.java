70a54eb817abf1944381ad9f5557ab98d2bf4425
Dmitrii Bit-Rigu <daxudo@gmail.com>
2023-03-10 00:46:22 +0200

    Solving the tasks.

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a = (char) 97;
+        char c = (char) 99;
+        char h = (char) 104;
+        char r = (char) 114;
+        System.out.println(a +" " + c + " " + h + " " + r);
     }
 }
